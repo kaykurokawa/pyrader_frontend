4d17837a3cd0496fc8219c29d0c0aee9fa0d7570
joeysk2012 <kurokaw1@gmail.com>
2018-07-15 14:00:21 -0700

    apply middleware was wrong

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,4 @@
-import { applyMiddleware } from "../../../../../.cache/typescript/2.8/node_modules/redux";
+import { applyMiddleware } from 'redux';
 import logger from './logger'
 import thunkMiddleware from 'redux-thunk'
 
